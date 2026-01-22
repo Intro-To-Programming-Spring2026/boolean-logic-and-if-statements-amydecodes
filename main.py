23d009fdfe6efe00aba9e01ce05b53729ce9f86b
Amy Ramos <armayorga.saca@gmail.com>
2026-01-22 12:16:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-input ("How many doughnuts did you eat?: ")
-int(input("Enter a number: >= 2"))
-if value >= 2:
-     print ("Gluttony is a sin.")  
-if value <= 2:
-     print ("That was very considerate of you.")
+doughnuts = int(input("Enter how many doughnuts you ate: "))
+
+if doughnuts >= 2: 
+    print ("Gluttony is a sin.")  
+if doughnuts < 2: 
+    print ("That was very considerate of you.")
